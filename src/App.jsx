afe92b84572008ca7d71a0deb3ef321f8a8db915
hj010 <80059966+hj010@users.noreply.github.com>
2025-05-14 21:20:01 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -112,7 +112,7 @@ function AuthPages({ isAuthenticated, setIsAuthenticated }) {
     }
   
     try {
-      const response = await fetch(`http://127.0.0.1:5000/cybedefender/forgot-password`, {
+      const response = await fetch(`${process.env.REACT_APP_CYBEDEFENDER_AI_URL}/cybedefender/forgot-password`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -211,7 +211,7 @@ function AuthPages({ isAuthenticated, setIsAuthenticated }) {
     };
 
     try {
-      const response = await fetch(`http://127.0.0.1:5000/cybedefender/signup`, {
+      const response = await fetch(`${process.env.REACT_APP_CYBEDEFENDER_AI_URL}/cybedefender/signup`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
